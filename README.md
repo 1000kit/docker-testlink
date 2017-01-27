f1953dcd25f9ef0d816112e731631d4db1bed424
tasgit <tasgit@1000kit.org>
2017-01-27 14:56:18 +0100

    Add docker compose for easy start enable restart of database

--- README.md
+++ README.md
@@ -24,15 +24,23 @@ $ docker run --name=testlink -d -p 8080:8080 -e TESTLINK_ADMIN_PASSWORD=<passwor
 for persistence use a data volume and run:
 * data volume container:
 ~~~~
-$ docker run --name=testlinkdb-data -v /var/lib/mysql 1000kit/testlink true
+$ docker run --name=testlinkdb -v /var/lib/mysql 1000kit/testlink true
 ~~~~
 
 * testlink using persistent data volume:
 ~~~~
-$ docker run --name=testlink -d -p 8080:8080 --volumes-from=testlinkdb-data -e TESTLINK_ADMIN_PASSWORD=<password> 1000kit/testlink
+$ docker run --name=testlink -d -p 8080:8080 --volumes-from=testlinkdb -e TESTLINK_ADMIN_PASSWORD=<password> 1000kit/testlink
 ~~~~
 
 * alternative import mysql backup data (name: mysql_import.sql.gz):
 ~~~~
 $ docker run --name=testlink -d -p 8080:8080 -v <localdir>/mysql_import.sql.gz:/import/mysql_import.sql.gz -e TESTLINK_ADMIN_PASSWORD=<password> 1000kit/testlink
 ~~~~
+
+* or just easy run docker compose
+1. paste mysql_import.sql.gz in `/opt/volumes/1000kit/testlink/import`
+2. download docker-compose.yml from github: `https://raw.githubusercontent.com/1000kit/docker-testlink/master/docker-compose.yml`
+
+~~~~
+$ docker-compose up
+~~~~ 
--- install/start_testlink.sh
+++ install/start_testlink.sh
@@ -3,39 +3,48 @@
 
 TL_ADMIN_PWD=${TESTLINK_ADMIN_PASSWORD:-'testlink'}
 
+INITDone=false
+if [ -d "/var/lib/mysql/mysql" ]; then
+ INITDone=true
+fi
 echo "==> start mysqld"
 gosu root chown mysql:mysql /docker-entrypoint.sh
 gosu mysql /docker-entrypoint.sh mysqld_safe | tee /tmp/mysqld_console.log 2>&1 &
 
 sleep 5
 
-echo "==> import data"
-if [ -e /import/mysql_import.sql.gz ]; then
-	zcat /import/mysql_import.sql.gz > /tmp/mysql_import
-	gosu mysql mysql -u root -ptestlink  --one-database testlink < /tmp/mysql_import
+if [  ${INITDone} == "false" ]; then
+	echo "==> initialize databse"
+	if [ -e /import/mysql_import.sql.gz ]; then
+		zcat /import/mysql_import.sql.gz > /tmp/mysql_import
+		gosu mysql mysql -u root -ptestlink  --one-database testlink < /tmp/mysql_import
+		
+	else
+		echo "no data to import found in /import/mysql_import.sql.gz"
+		gosu mysql mysql -u testlink -ptestlink -Dtestlink < /var/www/html/testlink/install/sql/mysql/testlink_create_tables.sql
+		gosu mysql mysql -u testlink -ptestlink -Dtestlink < /var/www/html/testlink/install/sql/mysql/testlink_create_default_data.sql
+	fi
 	
-else
-	echo "no data to import found in /import/mysql_import.sql.gz"
-	gosu mysql mysql -u testlink -ptestlink -Dtestlink < /var/www/html/testlink/install/sql/mysql/testlink_create_tables.sql
-	gosu mysql mysql -u testlink -ptestlink -Dtestlink < /var/www/html/testlink/install/sql/mysql/testlink_create_default_data.sql
-fi
-
-
-echo "==> upgrade DB"
-#cd /var/www/html/testlink/install/sql/alter_tables
-#for dir in 1.9.1[5-6]; do
+	echo "==> upgrade DB"
+	#cd /var/www/html/testlink/install/sql/alter_tables
+	#for dir in 1.9.1[5-6]; do
 	#	echo $dir
 	#echo db_schema_update.sql
 	#gosu mysql mysql -u root -ptestlink  --one-database testlink < ${dir}/mysql/DB.${dir}/step1/db_schema_update.sql
 	#echo z_final_step.sql
 	#gosu mysql mysql -u root -ptestlink  --one-database testlink < ${dir}/mysql/DB.${dir}/stepZ/z_final_step.sql
-#done	
+	#done
 
-echo ${TL_ADMIN_PWD}
-PASS=$(echo -n ${TL_ADMIN_PWD} | md5sum | awk '{print $1}')
+	echo ${TL_ADMIN_PWD}
+	PASS=$(echo -n ${TL_ADMIN_PWD} | md5sum | awk '{print $1}')
+	
+	echo "update users set password=\"${PASS}\" where login=\"admin\"" > /tmp/updatePass
+	gosu mysql mysql -u root -ptestlink  --one-database testlink < /tmp/updatePass
+				
+else
+	echo "no init of db - restart detected"
+fi
 
-echo "update users set password=\"${PASS}\" where login=\"admin\"" > /tmp/updatePass
-gosu mysql mysql -u root -ptestlink  --one-database testlink < /tmp/updatePass
 
 
 echo "==> START apache server ..."
